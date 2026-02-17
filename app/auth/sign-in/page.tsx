33e8eb83f1c4e21635cbc73baa2ba613d88be865
Franco <franconcarchedi@gmail.com>
2026-02-17 04:03:07 -0300

    fix: useSearchParams with Suspense boundary

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { AuthForm } from "../components/auth-form";
 
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <AuthForm typeForm="login" />
+        <Suspense>
+          <AuthForm typeForm="login" />
+        </Suspense>
       </div>
     </div>
   );
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { AuthForm } from "../components/auth-form";
 
 export default function RegisterPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <AuthForm typeForm="register"/>
+        <Suspense>
+          <AuthForm typeForm="register"/>
+        </Suspense>
       </div>
     </div>
   );
